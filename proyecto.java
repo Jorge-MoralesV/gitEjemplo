51f48be7859b1b2402a9e7ad5d3f85cf7a77ea35
Jorge-MoralesV <jorgemoralesv99@gmail.com>
2021-11-10 23:18:16 -0600

    commit

--- proyecto.java
+++ proyecto.java
@@ -1,10 +1,8 @@
 import java.util.Scanner;
-
-import javax.swing.plaf.synth.SynthRootPaneUI;
 public class proyecto{
 public static void main(String args []){
 Scanner leer = new Scanner(System.in);
-int a, b;    
+int a, b;
 System.out.println("Primer numero");
 a=leer.nextInt();
 System.out.println("Segundo numero");
